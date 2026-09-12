c2413202ab58dbb81103ac20d10ba7279ff888ac
MTkhai <kcott.maitrungkhai@gmail.com>
2026-09-12 12:13:34 +0000

    Sep 12, 2026, 7:13 PM

--- engine/modules/coin.js
+++ engine/modules/coin.js
@@ -3,7 +3,7 @@
    ========================================================================== */
 
 import { historyManager } from '../history.js';
-import { sfx } from '../utils/sfx_dice.js'; // Import SFX
+import { sfx } from '../utils/sfx_dice.js';
 
 export default class CoinModule {
     constructor() {
